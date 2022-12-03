7b898bbb1d2fe12d3b78f809cdaaad3a57571162
chimchimster <jorddyishere@gmail.com>
2022-12-03 15:45:27 +0500

    added dynamic creating new page ability

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:title>', views.entry, name='entry'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('new', views.new, name='new'),
 
 ]
 handler404 = 'encyclopedia.views.page_not_found'
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,6 +7,12 @@ from . import util
 class InputForm(forms.Form):
     input_form = forms.CharField(label='', max_length=100)
 
+class CreatePageForm(forms.Form):
+    title = forms.CharField(label='Please add title', min_length=10, max_length=100)
+    text_area = forms.CharField(label='Please add some text', widget=forms.Textarea(
+        attrs={'rows': 1, 'cols': 10}), min_length=50, max_length=5000)
+
+
 def page_not_found(request, exception):
     return render(request, 'encyclopedia/404.html', status=404)
 
@@ -44,12 +50,45 @@ def search(request):
                 if not substrings:
                     content = 'Requested Page is not Found on Server'
                     return render(request, 'encyclopedia/404.html', {
+                        'title': data,
                         'content': content,
+                        'form': form_instance,
                     })
                 else:
                     return render(request, 'encyclopedia/index.html', {
                         'entries': substrings,
+                        'form': form_instance,
                     })
 
+
+def new(request):
+    form = InputForm()
+    if request.method == 'POST':
+        form_page = CreatePageForm(request.POST)
+        if form_page.is_valid():
+            title = form_page.cleaned_data.get('title')
+            text_area = form_page.cleaned_data.get('text_area')
+            if title not in util.list_entries():
+                util.save_entry(title, text_area)
+                md_text = util.get_entry(title)
+                html_text = markdown.markdown(md_text)
+                return render(request, 'encyclopedia/entry.html', {
+                    'title': title,
+                    'form': form,
+                    'content': html_text,
+            })
+            else:
+                content = 'Page is Already Exists!'
+                return render(request, 'encyclopedia/entry.html', {
+                    'content': content,
+                    'form': form,
+                })
+    else:
+        form_page = CreatePageForm()
+        return render(request, 'encyclopedia/new.html', {
+            'form': form,
+            'form_text': form_page
+        }
+                      )
 
 
