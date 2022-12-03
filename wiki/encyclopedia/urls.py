83d69734e9baee7715180ef9f615426316bde68d
chimchimster <jorddyishere@gmail.com>
2022-12-03 10:33:40 +0500

    added dynamic links to index.html file

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('wiki/<str:title>', views.render_entry, name='render_entry')
+    path('wiki/<str:title>', views.render_entry, name='render_entry'),
+
 ]
 handler404 = 'encyclopedia.views.page_not_found'
